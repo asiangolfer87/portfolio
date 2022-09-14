2d1d11f07248c0f80e104bca350d0e96b9ec36c9
asiangolfer87 <dansidiki@live.com>
2022-09-14 17:15:25 -0500

    initial commit

---
+++ portfolio/src/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {      
+  return ( 
+    <div>
+        <div>
+            <p>Dan</p>
+        </div>
+        <div>
+            <p>About</p>
+            <p>Experience</p>
+             <p>Work</p> 
+             <p>Contact</p> 
+        </div>
+    </div>
+    
+  )
+}
+
+export default Navbar 
